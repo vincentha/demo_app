e40f84124fa7132300b1c23b8aec0bf54ca734dc
vincentha <vincent.ha@gushcloud.com>
2011-09-10 16:21:04 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1e486717adb1ed142c41ca1df1283afdd699a58a1be34acdc5d44a5e535b8c2129a002c2afcd9cca86a61e29dee01f5351fc9f98a6a9b1e17e5737d5f0f02706'
